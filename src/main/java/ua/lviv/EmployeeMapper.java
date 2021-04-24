c0839e1ba05ed531afd6bf62429fbb2ee92deca4
JuliaZayka <julia.zayka18@gmail.com>
2021-04-24 18:49:13 +0300

    no message

---
+++ src/main/java/ua/lviv/EmployeeMapper.java
@@ -0,0 +1,15 @@
+package ua.lviv;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class EmployeeMapper {
+	public static Employee map(ResultSet result) throws SQLException {
+		int id = result.getInt("id");
+		String firstName = result.getString("first_name");
+		String lastName = result.getString("last_name");
+		
+		return new Employee(id, firstName, lastName);
+	}
+
+}
